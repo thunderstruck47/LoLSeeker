666f4a1dacff75f57bb1416d8f6c382d4b8192f4
thunder <vgerov93@yahoo.com>
2014-05-18 18:43:06 +0100

    added simple static pages tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #adding capybara gem
+  config.include Capybara::DSL
 end
