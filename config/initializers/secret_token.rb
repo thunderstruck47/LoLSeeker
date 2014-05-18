f9c11a895c711e04f8a19f2975e952aac0754dbe
thunder <vgerov93@yahoo.com>
2014-05-18 15:49:41 +0100

    Initial commit, using RSPEC as TDE

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoLSeeker::Application.config.secret_key_base = 'd0de1b75aa9ed1f86150e166c0506c830504b12b30dc15099be5bf2212b91803347d1c8b91a300c68da3afdf310869eb04b706ceb229d9d82d3cd12c319027a5'
