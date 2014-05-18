2138ff51a1336a9f24c8e86374739a6738ceb46d
thunder <vgerov93@yahoo.com>
2014-05-18 18:25:27 +0100

    added staticpages controller and simple default layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 LoLSeeker::Application.routes.draw do
-  get "users/new"
+  get "static_pages/home"
+  get "static_pages/seekers"
+  get "static_pages/register"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
